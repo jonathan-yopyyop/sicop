a1741836b9307562cf15e4a7cc1492da2ee0853b
Jonathan Urzola Maldonado <jonathan.urzola@outlook.com>
2023-09-27 09:25:15 -0500

    ALLOWED_HOSTS *

--- config/settings/local.py
+++ config/settings/local.py
@@ -11,7 +11,7 @@ SECRET_KEY = env(
     default="9DCUtXaPH8IHlRxyQxyeoBRQM1P4RTWUHigFKcleeZTOmUIvnLNq41BhGAON6O0g",
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1", "*"]
 
 # CACHES
 # ------------------------------------------------------------------------------
