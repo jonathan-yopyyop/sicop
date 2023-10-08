1f93eaa078ea5f8ac44210ccf6443d7930844c86
Jonathan Urzola Maldonado <jonathan.urzola@outlook.com>
2023-10-08 14:09:08 -0500

    All authentication and permissions logic were developed

---
+++ sicop/users/views/__init__.py
@@ -0,0 +1,18 @@
+from sicop.users.views.groups import GroupCreateView, GroupDetailView, GroupListView, GroupUpdateView  # noqa
+from sicop.users.views.registration import (  # noqa
+    ArtdPasswordResetForm,
+    LoginView,
+    ResetPasswordMessageView,
+    ResetPasswordView,
+    SignupView,
+    UserPasswordChangeView,
+    UserProfileView,
+    logout_view,
+)
+from sicop.users.views.users import (  # noqa
+    UserCreateView,
+    UserListView,
+    UserProfileUpdateView,
+    UserUpdateView,
+    UsetDetailView,
+)
