358ee029565d9cfd0f6ff0b0190547a5ed39372e
Jonathan Urzola Maldonado <jonathan.urzola@outlook.com>
2023-12-13 13:19:05 -0500

    Remove option from budget decrease transactions

--- sicop/budget/admin.py
+++ sicop/budget/admin.py
@@ -167,3 +167,13 @@ class BudgetDecreaseTransactionAdmin(admin.ModelAdmin):
         "created_at",
         "updated_at",
     )
+
+    # remove delete, add, change
+    def has_add_permission(self, request, obj=None):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
