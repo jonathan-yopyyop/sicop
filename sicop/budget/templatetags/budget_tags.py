4856f55d256e331cd9e21b55b22d3486387ad62b
Jonathan Urzola Maldonado <jonathan.urzola@outlook.com>
2023-12-19 22:34:06 -0500

    Some changes

--- sicop/budget/templatetags/budget_tags.py
+++ sicop/budget/templatetags/budget_tags.py
@@ -10,4 +10,8 @@ def sum_2_values(value_1, value):
 
 @register.simple_tag
 def rest_2_values(value_1, value):
-    return float(value_1) - float(value)
+    rest = float(value_1) - float(value)
+    if rest < 0:
+        return 0
+    else:
+        return float(value_1) - float(value)
