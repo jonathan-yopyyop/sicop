70ce551c54b4d8868e59a24a9a675ff7152a38da
Jonathan Urzola Maldonado <jonathan.urzola@outlook.com>
2023-12-21 18:47:12 -0500

    Enable delete for transaction log

--- sicop/budget/admin.py
+++ sicop/budget/admin.py
@@ -176,8 +176,8 @@ class BudgetDecreaseTransactionAdmin(admin.ModelAdmin):
     def has_change_permission(self, request, obj=None):
         return False
 
-    def has_delete_permission(self, request, obj=None):
-        return False
+    # def has_delete_permission(self, request, obj=None):
+    #     return False
 
 
 @admin.register(ProvisionCartApproval)
