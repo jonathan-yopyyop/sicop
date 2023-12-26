bde02bba11014663de07a426fa028a74dded5245
Jonathan Urzola Maldonado <jonathan.urzola@outlook.com>
2023-12-26 15:48:54 -0500

    Rejected was added to a provision approval

--- config/settings/base.py
+++ config/settings/base.py
@@ -49,16 +49,16 @@ LOCALE_PATHS = [str(BASE_DIR / "locale")]
 # https://docs.djangoproject.com/en/dev/ref/settings/#databases
 DATABASES = {"default": env.db("DATABASE_URL")}
 DATABASES["default"]["ATOMIC_REQUESTS"] = True
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "sicop",
-        "USER": "myuser",
-        "PASSWORD": "mypass",
-        "HOST": "localhost",
-        "PORT": "5432",
-    }
-}
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": "sicop",
+#         "USER": "myuser",
+#         "PASSWORD": "mypass",
+#         "HOST": "localhost",
+#         "PORT": "5432",
+#     }
+# }
 # https://docs.djangoproject.com/en/stable/ref/settings/#std:setting-DEFAULT_AUTO_FIELD
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
@@ -106,7 +106,6 @@ LOCAL_APPS = [
     "sicop.business_unit",
     "sicop.expense_concept",
     "sicop.expense_type",
-    "sicop.certificate",
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
