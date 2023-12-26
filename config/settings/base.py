986f0a7f91a619c6bbf53d47252ddbddbd4795eb
Jonathan Urzola Maldonado <jonathan.urzola@outlook.com>
2023-12-26 15:47:59 -0500

    Rejected was added to a provision approval

--- config/settings/base.py
+++ config/settings/base.py
@@ -47,8 +47,8 @@ LOCALE_PATHS = [str(BASE_DIR / "locale")]
 # DATABASES
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#databases
-# DATABASES = {"default": env.db("DATABASE_URL")}
-# DATABASES["default"]["ATOMIC_REQUESTS"] = True
+DATABASES = {"default": env.db("DATABASE_URL")}
+DATABASES["default"]["ATOMIC_REQUESTS"] = True
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
