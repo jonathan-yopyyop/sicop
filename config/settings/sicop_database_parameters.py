9c6cf2533c1ba2100b9b445b59e2dd1f39b6cf25
Jonathan Urzola Maldonado <jonathan.urzola@outlook.com>
2024-01-15 13:40:22 -0500

    Menu was fixed

--- config/settings/sicop_database_parameters.py
+++ config/settings/sicop_database_parameters.py
@@ -2,9 +2,11 @@ import environ
 
 env = environ.Env()
 environ.Env.read_env()
+# for production
 DATABASES = {"default": env.db("DATABASE_URL")}
 DATABASES["default"]["ATOMIC_REQUESTS"] = True
 
+# for local
 # DATABASES = {
 #     "default": {
 #         "ENGINE": "django.db.backends.postgresql",
