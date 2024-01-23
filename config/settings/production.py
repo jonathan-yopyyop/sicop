9a9363044450b1a7a6684084d84c669952a708b5
Jonathan Urzola Maldonado <jonathan.urzola@outlook.com>
2024-01-22 21:38:22 -0500

    Allowed host was updated

--- config/settings/production.py
+++ config/settings/production.py
@@ -6,7 +6,13 @@ from .base import env
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["sicop.maloka.org"])
+ALLOWED_HOSTS = env.list(
+    "DJANGO_ALLOWED_HOSTS",
+    default=[
+        "sicop.maloka.org",
+        "sicop.artd.com.co",
+    ],
+)
 
 # DATABASES
 # ------------------------------------------------------------------------------
