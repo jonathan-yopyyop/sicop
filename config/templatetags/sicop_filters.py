53d2fbb6ddc4f98bb01cba06e50e4e1fbf9941f9
Jonathan Urzola Maldonado <jonathan.urzola@outlook.com>
2024-02-01 10:11:27 -0500

    Some changes on proyect list

---
+++ config/templatetags/sicop_filters.py
@@ -0,0 +1,27 @@
+from django import template
+from django.utils.html import escape
+
+register = template.Library()
+
+
+@register.filter(name="multiline_text")
+def multiline_text(value, line_length=50):
+    """
+    Convierte un texto de una sola línea en un texto de varias líneas
+    con un máximo de caracteres por línea especificado.
+    """
+    words = value.split()
+    lines = []
+    current_line = ""
+
+    for word in words:
+        if len(current_line) + len(word) <= line_length:
+            current_line += " " + word
+        else:
+            lines.append(escape(current_line.strip()))
+            current_line = word
+
+    if current_line:
+        lines.append(escape(current_line.strip()))
+
+    return "<br>".join(lines)
