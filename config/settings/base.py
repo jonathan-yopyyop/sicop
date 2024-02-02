bc0b646c3213b21dfc6b7e16b1abddb16e136073
Jonathan Urzola Maldonado <jonathan.urzola@outlook.com>
2024-02-01 23:14:33 -0500

    Copy .env

--- config/settings/base.py
+++ config/settings/base.py
@@ -10,9 +10,9 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent.parent
 # sicop/
 APPS_DIR = BASE_DIR / "sicop"
 env = environ.Env()
-environ.Env.read_env()
+# environ.Env.read_env()
 
-READ_DOT_ENV_FILE = env.bool("DJANGO_READ_DOT_ENV_FILE", default=True)
+READ_DOT_ENV_FILE = env.bool("DJANGO_READ_DOT_ENV_FILE", default=False)
 if READ_DOT_ENV_FILE:
     # OS environment variables take precedence over variables from .env
     env.read_env(str(BASE_DIR / ".env"))
