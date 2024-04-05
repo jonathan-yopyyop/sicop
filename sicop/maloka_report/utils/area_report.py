4fd5676c590c8a7051668b001811429dee35ed25
Jonathan Urzola <jonathan@artd.com.co>
2024-04-05 14:02:38 -0500

    SF

--- sicop/maloka_report/utils/area_report.py
+++ sicop/maloka_report/utils/area_report.py
@@ -261,7 +261,7 @@ def get_budget_by_projects_in_area(area: Area):
                     "total_requested_budget": total_requested_budget,
                     "total_commiment": total_commiment,
                     "total_to_be_committed": total_by_engaded,
-                    "total_to_be_committed2": total_to_be_committed,
+                    "test": f"{total_to_be_committed} = {total_available_budget} - {total_commiment}",
                     "total_provisioned_amount": total_requested_budget,
                 },
                 "data": {
